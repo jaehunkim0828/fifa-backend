0b83b1b9e0c010b89d2ed3fe0cac78a2527ca040
JaeHun Kim <kkaa81@naver.com>
2022-07-16 01:24:45 +0900

    test

--- src/service/rank.service.ts
+++ src/service/rank.service.ts
@@ -81,7 +81,7 @@ export async function createRanksEvery() {
   const wait = (times: number) => new Promise((resolve) => setTimeout(resolve, times));
   while (true) {
     console.log(new Date().toLocaleString());
-    if (new Date().getMinutes() === 23 && new Date().getHours() === 4) {
+    if (new Date().getMinutes() === 25 && new Date().getHours() === 4) {
       console.log("come in");
       for (let i = 0; i < players.length; i += 1) {
         const playerArr: string[] = [];
