1f066a4d94f37ed41c22398e5fbe0c8318a55cb2
JaeHun Kim <kkaa81@naver.com>
2022-08-17 00:19:04 +0900

    /image/:spid method 주석 처리

--- src/routes/player.ts
+++ src/routes/player.ts
@@ -7,7 +7,7 @@ const playerRouter = express.Router();
 playerRouter.route("/add/season").get(playerController.updateSeason);
 playerRouter.route("/:id").get(playerController.getPlayerById);
 playerRouter.route("/spid/:name").get(playerController.getPlayerByName);
-playerRouter.route("/image/:spid").get(playerController.a);
+// playerRouter.route("/image/:spid").get(playerController.a);
 playerRouter.route("/count/:name").get(playerController.countAllPlayer);
 
 export default playerRouter;
