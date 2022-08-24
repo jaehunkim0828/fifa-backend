daba946dd4d56b1a08eefc57fe8bfb7e52eb69f9
JaeHun Kim <kkaa81@naver.com>
2022-08-24 10:20:20 +0900

    fix ERR_HTTP_HEADERS_SENT error (2)

--- src/controller/rank.controller.ts
+++ src/controller/rank.controller.ts
@@ -89,7 +89,7 @@ export async function getPlayerTotalScorecard(req: Request, res: Response, next:
   try {
     if (typeof spid === "string" && typeof po === "string") {
       const data = await rankService.getRankById(spid, po);
-      res.status(200).send(data[0]);
+      return res.status(200).send(data[0]);
     }
     throw new Error("unexpected error");
   } catch (err) {
