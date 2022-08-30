345fe973c5fef7cc89a6cffbafd1371f094cab6a
JaeHun Kim <kkaa81@naver.com>
2022-08-30 15:13:40 +0900

    fix sequelize logging

--- src/mysql/db.ts
+++ src/mysql/db.ts
@@ -7,4 +7,5 @@ const { host, username, database, password } = config.db;
 export const sequelize = new SQ.Sequelize(database, username, password, {
   host,
   dialect: "mysql",
+  logging: process.env.NODE_ENV === "production" ? false : true,
 });
