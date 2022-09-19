380503f350a72c8593b0af7f143fa1c640f71f9d
JaeHun Kim <kkaa81@naver.com>
2022-09-19 22:59:26 +0900

    install request-ip

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
-import { config } from "../config/config";
+
+import requestIp from "request-ip";
 import { sendInfoAtGmail } from "../external/mail";
 import { position } from "../InsertData";
 import { getFifaApi } from "../method";
@@ -91,7 +92,7 @@ dataRouter.route("/newPlayer").get(async (req: Request, res: Response, next: Nex
 
 dataRouter.route("/welcome").get(async (req: Request, res: Response, next: NextFunction) => {
   try {
-    var ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    var ip = requestIp.getClientIp(req);
     await sendInfoAtGmail("새로운 유저가 입장했습니다.", `${ip} 아이피에서 접속했습니다.`);
     res.status(200).send("welcome!!");
   } catch (err) {
